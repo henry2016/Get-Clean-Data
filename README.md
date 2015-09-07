ac5c0bbea467082747a785eee80c6afbd4c2aa83
henry2016 <h2008f@gmail.com>
2015-09-07 09:50:25 -0500

    initial commit; work in progress.

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # Get-Clean-Data
-Student project for the online Johns Hopkins/Coursera "Getting and Cleaning Data" course
+A student project for the online Coursera course,
+_Getting and Cleaning Data_,
+ presented by Jeff Leek, PhD, Roger D. Peng, PhD, and Brian Caffo, PhD,
+of Johns Hopkins University
+Bloomberg School of Public Health.
+
+
+The contents of the "UCI HAR Dataset" subdirectory include accelerometer data
+from the Samsung Galaxy S smartphone. A full description is 
+available at the site where the data was originally obtained:
+
+http://archive.ics.uci.edu/ml/datasets/Human+Activity+Recognition+Using+Smartphones
---
+++ run_analysis.R
@@ -0,0 +1,14 @@
+# This R script, "run_analysis.R", was written in September, 2015 by henry2016
+# as a programming assignment for an online "Getting and Cleaning Data" course.
+#
+# According to the assignment's instructions, this script should:
+#
+# 1. Merge the training and the test sets to create a single data set.
+# 2. Extract only the measurements on the mean and standard deviation for each
+#    measurement.
+# 3. Use descriptive activity names to name the activities in the data set.
+# 4. Appropriately labels the data set with descriptive variable names.
+# 5. From the data set in step 4, creates a second, independent tidy data set
+#    with the average of each variable for each activity and each subject.
+#
+#    The training and the test sets are in the "UCI HAR Dataset" folder.
